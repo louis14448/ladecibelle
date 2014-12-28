55c4ff40e39e9e9a47bda77ccbd4d6957cb3d014
louis14448 <louis.biancherin@gmail.com>
2014-12-28 22:38:50 +0100

    version initiale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ladecibelle::Application.config.secret_token = '047a6bbcb0769a3e40fd94edfbc77816396690e72b7d93adfe9186dcf0428406c1f45c4bb0678a898f0245b40a590685fbf0d2aeffb800b023ceab246853d7a0'
